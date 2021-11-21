50cc401a585a9ea37b8374c48a842e88446e5e9c
kmaslennikov <82436599+kmaslennikov@users.noreply.github.com>
2021-11-21 13:54:02 +0000

    v1

--- main.go
+++ main.go
@@ -10,24 +10,23 @@ func main() {
 	fmt.Scan(&height)
 	var countStars, countSpace int
 	var line string
+	var isStar bool
 	length := 1 + 2*(height-1) // количество символов в каждой строке
 	fmt.Println(length)
 
 	for i := 0; i < height; i++ {
-    line = ""
+		line = ""
 		countStars = 1 + 2*i
 		countSpace = (length - countStars) / 2 //количество пробелов с каждой стороны
-		for s := 0; s < countSpace; s++ {
-			line += " "
+		for j := 0; j < length; j++ {
+			isStar = j >= countSpace && j < countSpace+countStars
+			if isStar {
+				line += "*"
+			} else {
+				line += " "
+			}
 		}
-		for j := 0; j < countStars; j++ {
-			line += "*"
-		}
-
-		for s := 0; s <= countSpace; s++ {
-			line += " "
-		}
-    println(line)
+		println(line)
 	}
-	
+
 }
